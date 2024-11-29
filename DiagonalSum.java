85bbf03e79da89dd0c66ff67eeb00817cea92354
David k <sba24003@student.cct.ie>
2024-11-29 23:53:38 +0000

    added DiagonalSum class

---
+++ DiagonalSum.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DiagonalSum {
+    public int calculateDiagonalSum(int userInput) {
+        int[][] diagonalSumArray = new int[userInput][userInput];
+        Set<Integer> s = new HashSet<>();
+
+        int arrayValueCounter = 0;
+        int counter = 0;
+        int sum = 0;
+
+        // fills array with values 
+        for (int i = 0; i < diagonalSumArray.length; i++) {
+            for (int j = 0; j < diagonalSumArray[i].length; j++) {
+                diagonalSumArray[i][j] = arrayValueCounter;
+                arrayValueCounter++;
+            }
+        }
+        // adds unique diagonal values to set 
+        for (int i = 0; i < diagonalSumArray.length; i++) {
+            s.add(diagonalSumArray[i][counter]);
+            s.add(diagonalSumArray[i][(diagonalSumArray.length - 1) - counter]);
+            counter++;
+        }
+        // sums set
+        for (int value : s) {
+            sum += value;
+        }
+
+        System.out.println(sum);
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        DiagonalSum obj = new DiagonalSum();
+        Scanner reader = new Scanner(System.in); 
+
+        System.out.println("Enter matrix size (n*n): ");
+        int n = reader.nextInt(); 
+        reader.close();
+
+        obj.calculateDiagonalSum(n);
+    }
+}
