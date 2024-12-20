56c1b85e229f00d0a6da5e5731217bf7c6cf5408
David k <sba24003@student.cct.ie>
2024-12-20 17:52:45 +0000

    added comments to diagonalsum and changed validation to be greater than 1 so matrix is square.

--- src/DiagonalSum.java
+++ src/DiagonalSum.java
@@ -3,15 +3,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DiagonalSum {
+    // calculate the sum of the main diagonal and secondary diagonal
+
     public int calculateDiagonalSum(int[][] arrayInput) {
         int sum = 0;
-        int j = arrayInput.length;
+        int j = arrayInput.length; // stores the size of the matrix as it is defined as square
 
         for (int i = 0; i < j; i++) {
             sum += arrayInput[i][i];
             sum += arrayInput[i][j - 1 - i];
         }
 
+        // If the matrix size is odd, subtract the middle element as it is counted twice in both diagonals
         if (j % 2 == 1) {
             sum -= arrayInput[j / 2][j / 2];
         }
@@ -27,11 +30,11 @@ public class DiagonalSum {
         try {
             System.out.println("Enter single digit(n) for array size - n*n: ");
             int n = reader.nextInt();
-            if (n <= 0) {
-                throw new IllegalArgumentException("Please enter a number greater than zero.");
+            if (n <= 1) {
+                throw new IllegalArgumentException("Please enter a number greater than one.");
             }
 
-            int[][] userInput = new int[n][n];
+            int[][] userInput = new int[n][n]; // square array defined 
 
             System.out.println("Enter Array Elements:");
             for (int[] userInput1 : userInput) {
@@ -49,8 +52,9 @@ public class DiagonalSum {
             }
 
             obj.calculateDiagonalSum(userInput);
+
         } catch (IllegalArgumentException e) {
-            System.out.println("Please enter a size greater than zero.");
+            System.out.println("Please enter a size greater than one.");
         } catch (Exception e) {
             System.out.println("Please enter a valid array size.");
         } finally {
